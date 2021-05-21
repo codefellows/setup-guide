9e347e388387a041797f75639f1b12d2d52c5dc8
Brook Riggio <brook@codefellows.com>
2021-05-21 13:19:02 -0700

    Update dsa.sh
    Convert primary branch to `main`.

--- code-301/code-challenges/bin/dsa.sh
+++ code-301/code-challenges/bin/dsa.sh
@@ -2,7 +2,7 @@
 
 # Download and configure the DS&A Repo First
 downloadRepo() {
-  curl https://codeload.github.com/codefellows/data-structures-and-algorithms/legacy.tar.gz/master --output dsa.tar.gz
+  curl https://codeload.github.com/codefellows/data-structures-and-algorithms/legacy.tar.gz/main --output dsa.tar.gz
 
   tar zxvf dsa.tar.gz --strip 1
 }
@@ -13,11 +13,11 @@ cleanup() {
 
 ## Get our configs
 getConfigFiles() {
-  curl https://raw.githubusercontent.com/codefellows/setup-guide/master/code-301/configs/.editorconfig --output .editorconfig
-  curl https://raw.githubusercontent.com/codefellows/setup-guide/master/code-301/configs/.eslintignore --output .eslintignore
-  curl https://raw.githubusercontent.com/codefellows/setup-guide/master/code-301/configs/.eslintrc.json --output .eslintrc.json
-  curl https://raw.githubusercontent.com/codefellows/setup-guide/master/code-301/configs/.gitignore --output .gitignore
-  curl https://raw.githubusercontent.com/codefellows/setup-guide/master/code-301/configs/.markdownlint.json --output .markdownlint.json
+  curl https://raw.githubusercontent.com/codefellows/setup-guide/main/code-301/configs/.editorconfig --output .editorconfig
+  curl https://raw.githubusercontent.com/codefellows/setup-guide/main/code-301/configs/.eslintignore --output .eslintignore
+  curl https://raw.githubusercontent.com/codefellows/setup-guide/main/code-301/configs/.eslintrc.json --output .eslintrc.json
+  curl https://raw.githubusercontent.com/codefellows/setup-guide/main/code-301/configs/.gitignore --output .gitignore
+  curl https://raw.githubusercontent.com/codefellows/setup-guide/main/code-301/configs/.markdownlint.json --output .markdownlint.json
 
 }
 
