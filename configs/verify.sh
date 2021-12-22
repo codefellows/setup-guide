314192399c7d71746c06c7f55eb5756765763411
John Cokos <highlander.44@gmail.com>
2021-12-22 11:20:59 -0800

    Create verify.sh (#107)

---
+++ configs/verify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+main(){
+    if code --version >/dev/null && git --version >/dev/null && node --version >/dev/null && npm --version >/dev/null && eslint --version >/dev/null && tree --version >/dev/null && cat ~/.gitconfig >/dev/null ; then
+        echo -e "${GREEN}By gum, you've done it!"
+    else
+        echo -e "${RED}Something went wrong!"
+    fi
+}
+
+main
+
+## curl -s https://raw.githubusercontent.com/codefellows/setup-guide/main/configs/verify.sh | bash
