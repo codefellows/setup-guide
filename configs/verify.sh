4292aeb6156b6a3f123cb23cb5443261ce82fd27
John Cokos <highlander.44@gmail.com>
2022-01-06 18:16:36 -0800

    Resets terminal colors after script runs (#113)
    * Resets terminal colors after script runs
    
    * Update verify.sh

--- configs/verify.sh
+++ configs/verify.sh
@@ -3,13 +3,17 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 
 main(){
+
     if code --version >/dev/null && git --version >/dev/null && node --version >/dev/null && npm --version >/dev/null && eslint --version >/dev/null && tree --version >/dev/null && cat ~/.gitconfig >/dev/null ; then
         echo -e "${GREEN}By gum, you've done it!"
     else
         echo -e "${RED}Something went wrong!"
     fi
+    
+    tput init
 }
 
 main
 
-## curl -s https://raw.githubusercontent.com/codefellows/setup-guide/main/configs/verify.sh | bash
+## Full/Raw: curl -Ls https://raw.githubusercontent.com/codefellows/setup-guide/main/configs/verify.sh | bash
+## Actual: curl -Ls https://bit.ly/3qMWhbE | bash
